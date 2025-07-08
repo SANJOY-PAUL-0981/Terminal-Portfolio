6003437f61d70aff997c4c2ff3ea9167fb8dfcc3
SANJOY <paulsanjoy2923@gmail.com>
2025-07-08 09:20:35 +0530

    new things added

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function about(){
     
     term.bold.cyan('Hello World welcome to my portfolio 👨‍💻\n\n\n');
     term.bold('😺 Name: ').brightMagenta('Sanjoy Paul\n');
-    term.bold('❓ About: ').cyan('Just a beginner developer, Interested in building new things\n');
+    term.bold('❓ About: ').cyan('Just a Web Developer, Interested in Backend Dev & building\n');
     term.bold('🌐 Website: ').yellow('https://sanjoypaul.vercel.app/\n');
     term.bold('🐙 GitHub: ').blue('https://github.com/SANJOY-PAUL-0981\n');
     term.bold('🐥 Twitter: ').brightCyan('https://x.com/Sanj0yX\n');
@@ -32,8 +32,9 @@ function about(){
 }
 
 function skills(){
-    term.bold('✒️ Languages: ').blue.bold('🟦 C ').yellow.bold('🟨 JavaScript ').brightBlue.bold('🟦 CSS ').red.bold('🟧 HTML').blue.bold('🟦 React\n');
-    term.bold('🛠️ Tools & Frameworks: ').brightBlue.bold('🌐 VS Code ').brightRed.bold('🐙 Git & GitHub ').brightMagenta.bold('👾 Bootstraps ').blue.bold('🌐 Tailwind\n');
+    term.bold('✒️ Languages: ').blue.bold('🟦 C ').yellow.bold('🟨 JavaScript ').brightBlue.bold('🟦 CSS ').red.bold('🟧 HTML').blue.bold('🟦 Java\n');
+    term.bold('🛠️ Frameworks: ').blue.bold('🌐 ReactJS ').green.bold('🍃 NodeJS ').blue.bold('🚂 ExpressJS ').brightMagenta.bold('👾 Bootstraps ').blue.bold('🌐 Tailwind\n');
+    term.bold('🔧 Others: ').yellow.bold('🐧 Linux ').red.bold('🐙 Git & Github ').blue.bold('🌐 VS Code ').green.bold('🍃 MongoDB')
 }
 
 console.log(ASCII_LOGO);
